a27a962bd09585b0371b4172770246dc5c13656d
김백진 <rlaqorwls27@gmail.com>
2026-03-24 13:31:17 +0900

    최신화

--- server.js
+++ server.js
@@ -150,6 +150,8 @@ app.post("/api/homework/:character/:task", async (req, res) => {
 });
 
 // ================= 서버 실행 =================
-app.listen(3000, () => {
-  console.log("Server running: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
