9f9fd12f222ec168b7e31482cb7152c5dcb94f88
김백진 <rlaqorwls27@gmail.com>
2026-03-24 13:55:49 +0900

    최신화

--- server.js
+++ server.js
@@ -154,6 +154,11 @@ app.post("/api/homework/:character/:task", async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  console.log("REQ:", req.method, req.url);
+  next();
+});
+
 app.get("/test", (req, res) => {
   res.send("OK");
 });
