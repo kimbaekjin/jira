89e3d7fd9d8fc2ef175b80e8993bbfd273d53dcb
김백진 <rlaqorwls27@gmail.com>
2026-03-24 13:42:18 +0900

    최신화

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ import express from 'express';
 import cors from 'cors';
 import { Pool } from 'pg';
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ================= DB 연결 =================
 const pool = new Pool({
@@ -18,7 +22,7 @@ const pool = new Pool({
 
 // ================= 루트 =================
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ================= 캐릭터 API =================
