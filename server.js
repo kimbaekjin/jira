007d64cb1f6b381582271eefe0a9e5ed7308c92e
김백진 <rlaqorwls27@gmail.com>
2026-03-24 13:46:43 +0900

    최신화

--- server.js
+++ server.js
@@ -161,6 +161,6 @@ app.get("/test", (req, res) => {
 // ================= 서버 실행 =================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
 });
